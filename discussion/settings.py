fa5d3a403a3bd581b61a937cf436a95ff76a2b3e
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-22 11:25:11 +0700

    db connection and installed app

--- discussion/settings.py
+++ discussion/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'discussion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'discussion',
+        'USER': 'dbnam',
+        'PASSWORD': 'lorem',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
