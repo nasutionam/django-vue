9b75b4d79a1c50949a579996e8a0a3c5d7a79fbd
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-27 12:16:21 +0700

    added Example vue component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Example from "../views/Example.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
@@ -11,19 +13,20 @@ const routes = [
     component: Home
   },
   {
+    path: "/example",
+    name: "example",
+    component: Example
+  },
+  {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: About
   }
 ];
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
+  //base: process.env.BASE_URL,
   routes
 });
 
---
+++ frontend/src/views/Example.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="example">
+        <h1>This is an example page </h1>
+        <h2>{{ message }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "example",
+    data() {
+        return {
+            message: "Hello This is an example"
+        }
+    }    
+}
+</script>
+
+<style scoped>
+    h1 {
+        color: red;
+    }
+</style>
