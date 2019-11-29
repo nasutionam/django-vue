01ba002928ee8d0b4b6e0071cce25fdcde0e5efb
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-29 21:13:21 +0700

    added feature for check if user has been asnwer

--- frontend/src/views/Question.vue
+++ frontend/src/views/Question.vue
@@ -6,9 +6,31 @@
                 <h2> {{ question.content }} </h2>
                 <p class="mb-0">Posted by:
                     <span class="author-name">{{ question.author }}</span>
-                </p>
+                </p>    
                 <p> <small> {{ question.created_at }} </small></p>
             </div>
+
+            <div v-if="userHasAnswered">
+                <p class="answer-added" style="margin-left: 13px; color:lightgreen;">You've written an answer</p>
+            </div>
+            <div v-else-if="showForm">
+                <form class="card" @submit.prevent="onSubmit">
+                    <div class="card-header px-3">
+                        Answer the Question
+                    </div>
+                    <div class="card-block">
+                        <textarea rows="5" v-model="newAnswerBody" class="form-control" placeholder="Answer the Question"></textarea>
+                    </div>
+                    <div class="card-footer px-3">
+                        <button type="submit" class="btn btn-sm btn-success">Submit your answer</button>
+                    </div>
+                </form>
+                <p v-if="error" class="error mt-2">{{ error }}</p>
+            </div>
+            <div v-else>
+                <button style="margin-left: 10px;" class="btn btn-primary btn-sm" @click="showForm = true">Answer the Question</button>
+            </div>
+
             <div>
                 <p v-if="anyAnswer" class="mt-5" style="margin-left: 13px; color: gray;">The answers...</p>
                 <p v-else class="mt-5" style="margin-left: 13px; color: gray;">No answer...</p>
@@ -40,7 +62,11 @@ export default {
         return  {
             question: {},
             answers: [],
-            anyAnswer: true
+            anyAnswer: true,
+            newAnswerBody: null,
+            error: null,
+            userHasAnswered: false,
+            showForm: false
         }
     },
     methods: {
@@ -54,6 +80,7 @@ export default {
             apiService(endpoint)
                 .then(data => {
                     this.question = data;
+                    this.userHasAnswered = data.user_has_answered;
                     this.setPageTitle(data.content);
                 })
         },
@@ -66,6 +93,23 @@ export default {
                         this.anyAnswer = false;
                     }
                 })
+        },
+        onSubmit() {
+            if(this.newAnswerBody) {
+                let endpoint = `/api/questions/${this.slug}/answer/`;
+                apiService(endpoint, "POST", { body: this.newAnswerBody})
+                    .then(data => {
+                        this.answers.unshift(data);
+                    })
+                this.newAnswerBody = null;
+                this.showForm = false;
+                this.userHasAnswered = true;
+                if(this.error) {
+                    this.error = null;
+                }
+            } else {
+                this.error = "You cannot send an empty message";
+            }
         }
     },
     created() {
