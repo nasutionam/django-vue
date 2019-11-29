36b4cbf4c3dea34b01ca0c62f58afe2d7714826c
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-11-29 13:42:20 +0700

    fixed space problem and added the answer

---
+++ frontend/src/components/Answer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="answer-bord">
+            <p class="text-muted">
+            <strong>Answered by: {{ answer.author }}</strong>
+            </p>
+            <p>{{ answer.body }} </p>
+            <p><small>{{ answer.created_at }} </small></p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AnswerComponent",
+    props: {
+        answer: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+    .answer-bord {
+        border: 1px solid rgba(0,0,0,0.1);
+        padding: 10px;
+        margin-bottom: 10px;
+    }
+</style>
--- frontend/src/views/Question.vue
+++ frontend/src/views/Question.vue
@@ -9,13 +9,20 @@
                 </p>
                 <p> <small> {{ question.created_at }} </small></p>
             </div>
-            
+            <div>
+                <p v-if="anyAnswer" class="mt-5" style="margin-left: 13px; color: gray;">The answers...</p>
+                <p v-else class="mt-5" style="margin-left: 13px; color: gray;">No answer...</p>
+
+
+                <AnswerComponent v-for="(answer, index) in answers" :key="index" :answer="answer" />
+            </div>
         </div>    
     </div>
 </template>
 
 <script>
 import { apiService } from "../common/api.service";
+import AnswerComponent from "@/components/Answer.vue";
 
 export default {
     name: "Question",
@@ -25,9 +32,15 @@ export default {
             required: true
         }
     },
+    components: {
+         AnswerComponent
+    },
+
     data() {
         return  {
-            question: {}
+            question: {},
+            answers: [],
+            anyAnswer: true
         }
     },
     methods: {
@@ -43,11 +56,22 @@ export default {
                     this.question = data;
                     this.setPageTitle(data.content);
                 })
+        },
+        getQuestionAnswers() {
+            let endpoint = `/api/questions/${this.slug}/answers/`;
+            apiService(endpoint)
+                .then(data => {
+                    this.answers = data.results;
+                    if(this.answers.length <= 0 ){
+                        this.anyAnswer = false;
+                    }
+                })
         }
     },
     created() {
-        this.getQuestionData()
-        console.log(this.question)
+        this.getQuestionData();
+        this.getQuestionAnswers();
+        console.log(this.question);
     }
 }
 </script>
@@ -63,4 +87,5 @@ export default {
     margin-top: 20px;
     color: gray;
   }
+  
 </style>
